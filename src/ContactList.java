b940171f6a81b727554d611a1166de29eab992e1
Daniel Name <daniel.v.name@gmail.com>
2022-10-27 09:49:24 -0500

    completed planning pseudocode

--- src/ContactList.java
+++ src/ContactList.java
@@ -3,3 +3,27 @@ public class ContactList {
 
     }
 }
+/*
+interface define functions:
+ 1)Read
+    a) search and display an individual contact
+    b) display all
+    - extra c) display all that contain the search criteria  extra -
+ 2) add
+    a) create new contact from nothing
+        -add contact to its own line in txt file
+   -extra  b) add multiple (extra, but very doable, justin showed us how)  extra -
+   !important  c) create txt file
+ 3) delete
+    a) delete one
+    b) delete all
+ - extra 4) update extra-
+
+ in main:
+ 1) have a main menu that presents options to the user
+    a) crud
+    b) exit
+        I) before exiting rewrite contact file
+        II) this is the only endpoint to UI (loop where this is the only break)
+ 2) call for user input (scanner)
+ */
